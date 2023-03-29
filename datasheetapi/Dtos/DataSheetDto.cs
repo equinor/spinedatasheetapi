036d8a99cfda31de1578917592efe231b97c51e7
Tord Austad <toaus@equinor.com>
2023-03-29 12:43:38 +0200

    feat: Add missing properties to datsheet model

--- datasheetapi/Dtos/DataSheetDto.cs
+++ datasheetapi/Dtos/DataSheetDto.cs
@@ -3,6 +3,12 @@ namespace datasheetapi.Dtos;
 public class DatasheetDto
 {
     public Guid Id { get; set; }
+    public Guid ProjectId { get; set; }
+    public string? TagNo { get; set; }
+    public string? Description { get; set; }
+    public string? Category { get; set; }
+    public string? Area { get; set; }
+    public string? Dicipline { get; set; }
     public PurchaserRequirement? PurchaserRequirement { get; set; }
     public SupplierOfferedProduct? SupplierOfferedProduct { get; set; }
 }
--- datasheetapi/Models/DataSheet.cs
+++ datasheetapi/Models/DataSheet.cs
@@ -4,6 +4,11 @@ public class Datasheet
 {
     public Guid Id { get; set; }
     public Guid ProjectId { get; set; }
+    public string? TagNo { get; set; }
+    public string? Description { get; set; }
+    public string? Category { get; set; }
+    public string? Area { get; set; }
+    public string? Dicipline { get; set; }
     public PurchaserRequirement? PurchaserRequirement { get; set; }
     public SupplierOfferedProduct? SupplierOfferedProduct { get; set; }
 }
