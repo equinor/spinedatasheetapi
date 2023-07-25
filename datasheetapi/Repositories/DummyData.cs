701669208b95b613074d6eaaa08bcfeb6b495841
Tord Austad <toaus@equinor.com>
2023-07-25 13:38:33 +0200

    Exclude dummy data from code coverage

--- datasheetapi/Repositories/DummyData.cs
+++ datasheetapi/Repositories/DummyData.cs
@@ -1,4 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace datasheetapi.Repositories;
+
+[ExcludeFromCodeCoverage]
 public static class DummyData
 {
     public static void InitializeDummyData()
