432aa6a8a669a4c746f4a78bdfc51ae735afd0c0
Tord Austad <toaus@equinor.com>
2023-09-19 09:57:35 +0200

    get orgchart members from fusion context

--- datasheetapi/Services/Fusion/FusionPeopleService.cs
+++ datasheetapi/Services/Fusion/FusionPeopleService.cs
@@ -29,40 +29,26 @@ public class FusionPeopleService
 
     public async Task<List<FusionPersonResultV1>> GetAllPersonsOnProject(string projectMasterId, string search, int top, int skip)
     {
-        // var context = await _fusionContextResolver.ResolveContextAsync(projectMasterId);
-        // if (context == null)
-        // {
-        //     throw new Exception($"Could not resolve context for project {projectMasterId}");
-        // }
-        // var contextRelations = await _fusionContextResolver.GetContextRelationsAsync(Guid.Parse(projectMasterId));
+        var contextRelations = await _fusionContextResolver.GetContextRelationsAsync(Guid.Parse(projectMasterId));
 
-        string orgChartId = string.Empty;
+        string? orgChartId = string.Empty;
 
-        // foreach (var contextRelation in contextRelations)
-        // {
-        //     Console.WriteLine("ContextRelationType: " + contextRelation.Type);
-        //     Console.WriteLine("ContextRelationId: " + contextRelation.Id);
-        //     Console.WriteLine("ContextRelationTitle: " + contextRelation.Title);
-        //     Console.WriteLine("ContextRelationExternalId: " + contextRelation.ExternalId);
+        foreach (var contextRelation in contextRelations)
+        {
+            if (contextRelation.Type == FusionContextType.OrgChart)
+            {
+                orgChartId = contextRelation.ExternalId?.ToString();
+            }
+        }
 
+        if (string.IsNullOrEmpty(orgChartId))
+        {
+            return new List<FusionPersonResultV1>();
+        }
 
-        //     if (contextRelation.Type == FusionContextType.OrgChart)
-        //     {
-        //         orgChartId = contextRelation.Id.ToString();
-        //         Console.WriteLine("OrgChartId: " + orgChartId);
-        //     }
-        // }
-
-        // Console.WriteLine("Context: " + context);
-
-        Console.WriteLine("FusearchContextId: " + projectMasterId);
-        Console.WriteLine("Search: " + search);
         var fusionSearchObject = new FusionSearchObject
         {
-            Filter = $"positions/any(p: p/isActive eq true and p/project/id eq 'c8bf14bc-d649-4d57-90d9-e4a0f78298b4' and p/contract eq null)",
-            // Filter = $"positions/any(p: p/project/id eq '${projectMasterId}')",
-            // Filter = $"positions/any(p: p/isActive eq true and p/project/id eq '${projectMasterId}' and p/contract eq null)",
-            // Filter = $"positions/any(p: p/isActive eq true and p/contract eq null)",
+            Filter = $"positions/any(p: p/isActive eq true and p/project/id eq '{orgChartId}' and p/contract eq null)",
             Top = 100,
             Skip = skip
         };
@@ -70,7 +56,6 @@ public class FusionPeopleService
         if (!string.IsNullOrEmpty(search))
         {
             fusionSearchObject.Filter += $" and {AddSearch(search)}";
-            // fusionSearchObject.Filter += $"{AddSearch(search)}";
         }
 
         return await QueryFusionPeopleService(fusionSearchObject);
