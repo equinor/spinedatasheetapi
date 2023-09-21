e05a48e5642840542ee5d4f507b63df1601339a3
Tord Austad <toaus@equinor.com>
2023-09-21 10:22:10 +0200

    Move dto mapping to controller, remove unnecessary service

--- datasheetapi/Controllers/UserTagsController.cs
+++ datasheetapi/Controllers/UserTagsController.cs
@@ -17,18 +17,27 @@ namespace datasheetapi.Controllers;
 public class UserTagsController : ControllerBase
 {
     private readonly ILogger<UserTagsController> _logger;
-    private readonly IUserTagService _userTagService;
+    private readonly IFusionPeopleService _fusionPeopleService;
 
-    public UserTagsController(ILoggerFactory loggerFactory, IUserTagService userTagService)
+    public UserTagsController(ILoggerFactory loggerFactory, IFusionPeopleService fusionPeopleService)
     {
         _logger = loggerFactory.CreateLogger<UserTagsController>();
-        _userTagService = userTagService;
+        _fusionPeopleService = fusionPeopleService;
     }
 
     [HttpGet("{fusionContextId}", Name = "GetUsersForProject")]
-    public async Task<ActionResult<List<UserTagDto>?>> GetUsersForProject(string fusionContextId, [FromQuery] string search = "", [FromQuery] int top = 20, [FromQuery] int skip = 0)
+    public async Task<ActionResult<List<UserTagDto>?>> GetUsersForProject(string fusionContextId, [FromQuery] string? search, [FromQuery] int top = 20, [FromQuery] int skip = 0)
     {
-        var fusionRepsonse = await _userTagService.GetUsersFromOrgChart(fusionContextId, search, top, skip);
-        return Ok(fusionRepsonse);
+        var fusionRepsonse = await _fusionPeopleService.GetAllPersonsOnProject(fusionContextId, search ?? "", top, skip);
+
+        var userTagDtos = fusionRepsonse.Select(fusionPersonResultV1 => new UserTagDto
+        {
+            AzureUniqueId = fusionPersonResultV1.AzureUniqueId,
+            DisplayName = fusionPersonResultV1.Name,
+            Mail = fusionPersonResultV1.Mail,
+            AccountType = fusionPersonResultV1.AccountType
+        }).ToList();
+
+        return userTagDtos;
     }
 }
--- datasheetapi/Program.cs
+++ datasheetapi/Program.cs
@@ -144,8 +144,6 @@ builder.Services.AddScoped<IRevisionContainerService, RevisionContainerService>(
 builder.Services.AddScoped<IFusionService, FusionService>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<IFusionPeopleService, FusionPeopleService>();
-builder.Services.AddScoped<IUserTagService, UserTagService>();
-
 
 builder.Services.AddSingleton<IFAMService, DummyFAMService>();
 
