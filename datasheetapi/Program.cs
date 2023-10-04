e2ce84c3085549e7f7eded5ca8320744a8967dcd
Tord Austad <toaus@equinor.com>
2023-10-04 10:06:12 +0200

    .

--- datasheetapi/Program.cs
+++ datasheetapi/Program.cs
@@ -150,8 +150,8 @@ builder.Services.AddScoped<IRevisionContainerService, RevisionContainerService>(
 builder.Services.AddScoped<IFusionService, FusionService>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<IFusionPeopleService, FusionPeopleService>();
-builder.Services.AddScoped<ReviewerTagDataReviewService>();
-builder.Services.AddScoped<ReviewerTagDataReviewRepository>();
+builder.Services.AddScoped<IReviewerTagDataReviewService, ReviewerTagDataReviewService>();
+builder.Services.AddScoped<IReviewerTagDataReviewRepository, ReviewerTagDataReviewRepository>();
 
 builder.Services.AddSingleton<IFAMService, DummyFAMService>();
 
