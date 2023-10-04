a08ffa2efb0a2b3f70c92b3719c673f420ae2073
Tord Austad <toaus@equinor.com>
2023-10-04 08:38:21 +0200

    .

--- datasheetapi/Models/ReviewerTagDataReview.cs
+++ datasheetapi/Models/ReviewerTagDataReview.cs
@@ -3,8 +3,6 @@ namespace datasheetapi.Models;
 public class ReviewerTagDataReview
 {
     public ReviewStatusEnum Status { get; set; }
-
-    // Relationships
     public Guid TagDataReviewId { get; set; }
     public Guid ReviewerId { get; set; }
 }
