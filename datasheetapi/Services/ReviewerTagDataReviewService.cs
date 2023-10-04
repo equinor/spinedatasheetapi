c8a7d7678c2278d01127da562bf143e04c15984d
Tord Austad <toaus@equinor.com>
2023-10-04 08:43:27 +0200

    .

--- datasheetapi/Services/ReviewerTagDataReviewService.cs
+++ datasheetapi/Services/ReviewerTagDataReviewService.cs
@@ -5,27 +5,25 @@ namespace datasheetapi.Services;
 
 public class ReviewerTagDataReviewService
 {
-    private readonly ILogger<ReviewerTagDataReviewService> _logger;
-    private readonly ITagDataService _tagDataService;
     private readonly ITagDataReviewService _reviewService;
-
-    private readonly ITagDataReviewRepository _reviewRepository;
 
     private readonly ReviewerTagDataReviewRepository _reviewerTagDataReviewRepository;
 
-    public ReviewerTagDataReviewService(ILoggerFactory loggerFactory, ITagDataReviewRepository reviewRepository,
-        ITagDataService tagDataService, ITagDataReviewService reviewService, ReviewerTagDataReviewRepository reviewerTagDataReviewRepository)
+    public ReviewerTagDataReviewService(
+        ITagDataReviewService reviewService,
+        ReviewerTagDataReviewRepository reviewerTagDataReviewRepository)
     {
-        _reviewRepository = reviewRepository;
-        _tagDataService = tagDataService;
-        _logger = loggerFactory.CreateLogger<ReviewerTagDataReviewService>();
         _reviewService = reviewService;
         _reviewerTagDataReviewRepository = reviewerTagDataReviewRepository;
     }
 
     public async Task<ReviewerTagDataReview> CreateReviewerTagDataReview(Guid reviewId, ReviewerTagDataReview review)
     {
-        if (!await _reviewService.AnyTagDataReview(reviewId)) { throw new NotFoundException($"Invalid reviewId - {reviewId}."); }
+        if (!await _reviewService.AnyTagDataReview(reviewId))
+        {
+
+            throw new NotFoundException($"Invalid reviewId - {reviewId}.");
+        }
 
         review.TagDataReviewId = reviewId;
 
