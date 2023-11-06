642511d2527fe8a45995542fe48bc20757f6919e
Tord Austad <toaus@equinor.com>
2023-11-06 10:37:59 +0100

    .

--- datasheetapi/Database/SaveSampleDataToDB.cs
+++ datasheetapi/Database/SaveSampleDataToDB.cs
@@ -16,19 +16,15 @@ public static class SaveSampleDataToDB
 
         var projects = DummyData.GetProjects();
         context.Projects.AddRange(projects);
-        //context.SaveChanges();
 
         var contracts = DummyData.GetContracts();
         context.Contracts.AddRange(contracts);
-        //context.SaveChanges();
 
         var containers = DummyData.GetContainers();
         context.Containers.AddRange(containers);
-        //context.SaveChanges();
 
         var containerReviews = DummyData.GetContainerReviews();
         context.ContainerReviews.AddRange(containerReviews);
-        //context.SaveChanges();
 
         DummyData.GetTagDatas();
         context.SaveChanges();
