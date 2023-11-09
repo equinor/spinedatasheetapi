64ba40ff4fb2b37b108c590a835cb9330ab101c6
Tord Austad <toaus@equinor.com>
2023-11-09 09:04:37 +0100

    .

--- datasheetapi/Services/ITagDataService.cs
+++ datasheetapi/Services/ITagDataService.cs
@@ -3,6 +3,6 @@ namespace datasheetapi.Services;
 public interface ITagDataService
 {
     Task<ITagData> GetTagDataByTagNo(string tagNo);
-    Task<List<ITagData>> GetTagDataByTagNos(List<string> tagNo);
+    Task<List<ITagData>> GetTagDataByTagNos(List<string> tagNos);
     Task<List<ITagData>> GetAllTagData();
 }
